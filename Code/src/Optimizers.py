eb8a6e85d222da0bd5c90a4c9f370e280e35946f
biancaziliotto <b.ziliotto@studenti.unipi.it>
2023-07-01 18:30:07 +0200

    modified:   Code/src/Optimizers.py

--- Code/src/Optimizers.py
+++ Code/src/Optimizers.py
@@ -360,7 +360,11 @@ class CG(Optimizer):
         """
         super().initialize(model, stopping_value, batch_size, alpha_l1, alpha_l2, verbose)
         
-        self.beta_type = beta_type
+        if beta_type in ['FR', 'HS+', 'PR+']:
+            self.beta_type = beta_type
+        else:
+            raise ValueError('Insert valid beta type (FR, HS+, PR+)')
+
         self.m1 = m1
         self.m2 = m2
         self.MaxFeval = MaxFeval
@@ -530,12 +534,18 @@ class CG(Optimizer):
         grad_params_flat = self._flatten(grad_params)
         last_grad_params_flat = self._flatten(self.last_grad_params)
         last_d_flat = self._flatten(self.last_d)
-       
-        if self.beta_type == "FR":
-            if np.linalg.norm(last_grad_params_flat) !=0 :
+
+        try: 
+            if self.beta_type == "FR":
                 beta = np.linalg.norm(grad_params_flat)**2/np.linalg.norm(last_grad_params_flat)**2
-            else:
-                beta = 0
+            elif self.beta_type == "HS+":
+                beta = np.max(0, np.dot(grad_params_flat, (grad_params_flat-last_grad_params_flat))/ \
+                          np.dot(last_d_flat, (grad_params_flat-last_grad_params_flat)))
+            else: #PR+
+                beta = np.max(0, np.dot(grad_params_flat, (grad_params_flat-last_grad_params_flat))/ \
+                          np.linalg.norm(last_grad_params_flat)**2)
+        except ZeroDivisionError:
+            beta = 0
 
         d = []
         for l in range(len(grad_params)):
