ba75005b309c70449934f133bbc62a149d55867f
Andrea Roncoli <115221482+DreRnc@users.noreply.github.com>
2023-07-05 10:43:46 +0200

    Updated EarlyStopping to be difference of steps.

--- Code/src/EarlyStopping.py
+++ Code/src/EarlyStopping.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class EarlyStopping():
 
-    def __init__ (self, patience = 100, tolerance = 1e-4):
+    def __init__ (self, mode = 'grad_norm'):
         """
         Initializes the early stopping criterion.
 
@@ -12,18 +12,19 @@ class EarlyStopping():
         tolerance (float): tolerance for the early stopping criterion
 
         """
-        self.patience = patience
-        self.tolerance = tolerance
+        if mode not in ['grad_norm', 'obj_tol']:
+            raise ValueError('mode must be either grad_norm or obj_tol')
+        self.mode = mode
 
-    def initialize (self):
+    def initialize (self, patience = 100, tolerance = 1e-4):
         """
         Initializes the early stopping criterion.
         
         """
+        self.patience = patience
+        self.tolerance = tolerance
         self._n_worsening_epochs = 0
-        self._best_objective = np.infty
-
-
+        self.last_obj = np.inf
 
     def __call__ (self, objective):
         """
@@ -38,13 +39,58 @@ class EarlyStopping():
         bool: True if the training should stop, False otherwise
         
         """
-        if objective < self._best_objective - self.tolerance:
-            self._best_objective = objective
-            self._n_worsening_epochs = 0
-        else:
+        if self.mode == 'grad_norm':
+            return self._check_grad_norm(objective)
+        elif self.mode == 'obj_tol':
+            return self._check_obj_tol(objective)
+        
+    def _check_obj_tol (self, objective):
+        """
+        Returns True if the training should stop, False otherwise.
+        
+        Parameters
+        ----------
+        objective (float): objective function value
+        
+        Returns
+        -------
+        bool: True if the training should stop, False otherwise
+        
+        """
+        if abs(objective - self.last_obj) < self.tolerance:
             self._n_worsening_epochs += 1
+        else:
+            self._n_worsening_epochs = 0
 
-        if self._n_worsening_epochs > self.patience:
+        self.last_obj = objective
+
+        if self._n_worsening_epochs >= self.patience:
             return True
         else:
             return False
+    
+    def _check_grad_norm (self, grad_norm):
+        """
+        Returns True if the training should stop, False otherwise.
+        
+        Parameters
+        ----------
+        objective (float): objective function value
+        
+        Returns
+        -------
+        bool: True if the training should stop, False otherwise
+        
+        """
+        if grad_norm < self.tolerance:
+            self._n_worsening_epochs += 1
+        else:
+            self._n_worsening_epochs = 0
+
+        if self._n_worsening_epochs >= self.patience:
+            return True
+        else:
+            return False
+        
+        
+        
